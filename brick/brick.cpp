9c9eb9bb52f8c234082b44f5722bbea8bdc6a50d
DimitriiTrater <dima1603040@gmail.com>
2023-01-09 19:47:02 +0200

    Add brick class

---
+++ brick/brick.cpp
@@ -0,0 +1,5 @@
+#include "brick.hpp"
+
+Brick::Brick(/* args */)
+{
+}
---
+++ brick/brick.hpp
@@ -0,0 +1,11 @@
+#ifndef BRICK_HPP
+#define BRICK_HPP
+class Brick
+{
+private:
+    /* data */
+public:
+    Brick(/* args */);
+    ~Brick();
+};
+#endif /*BRICK_HPP*/ 
